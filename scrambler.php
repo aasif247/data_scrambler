7f77ac04b1c8b74622126f6c5612d00a976cc084
aasif247 <47910560+aasif247@users.noreply.github.com>
2021-12-07 15:40:47 +0600

    key generated successfully

--- scrambler.php
+++ scrambler.php
@@ -1,3 +1,21 @@
+<?php
+include_once "function.php";
+$task ='encode';
+if(isset($_GET['task']) && $_GET['task']!=''){
+    $task = $_GET['task'];
+    // $task = $_GET['task'] ?? 'encode' //for php 7;
+}
+
+$key ='abcdefghijklmnopqrstuvwxyz1234567890';
+if('key'== $task){
+    $key_original = str_split($key);
+    shuffle($key_original);
+    $key = join('',$key_original);
+}else if(isset($_POST['key']) && $_POST['key'] !=''){
+    $key = $_POST['key'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,17 +51,17 @@
             <h2>Data Scrambler</h2>
             <p>Use this application to scramble your data</p>
             <p>
-                <a href="">Encode</a> |
-                <a href="">Decode</a> |
-                <a href="">Generate Key</a>
+                <a href="scrambler.php?task=encode">Encode</a> |
+                <a href="scrambler.php?task=decode">Decode</a> |
+                <a href="scrambler.php?task=key">Generate Key</a>
             </p>
         </div>
     </div>
     <div class="row">
         <div class="column column-60 column-offset-20">
-            <form method="POST">
+            <form method="POST" action="scrambler.php">
                 <label for="key">Key</label>
-                <input type="text" name="key" id="key">
+                <input type="text" name="key" id="key" <?php displayKey($key); ?> >
 
                 <label for="data">Data</label>
                 <textarea name="data" id="data"></textarea>
